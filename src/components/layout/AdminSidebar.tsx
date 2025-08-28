9126d57b34492d70118e31e91010440216c69f8c
Kuralarasan T <kuralmani1118@gmail.com>
2025-08-28 12:26:16 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/layout/AdminSidebar.tsx
+++ src/components/layout/AdminSidebar.tsx
@@ -39,7 +39,6 @@ export default function AdminSidebar() {
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/admin" className="flex items-center gap-2 font-semibold">
             <Logo />
-            <span className="">Admin Panel</span>
           </Link>
           <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
             <Bell className="h-4 w-4" />
