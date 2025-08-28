62c5a83c3eb8cb792bb9f0f8e9c4e970f9fb7f7d
Kuralarasan T <kuralmani1118@gmail.com>
2025-08-28 07:58:54 +0000

    enku exam write attenlike take etuka page full screnn vara nuuim start e

--- src/app/exams/[id]/take/layout.tsx
+++ src/app/exams/[id]/take/layout.tsx
@@ -1,3 +1,27 @@
+import Logo from "@/components/Logo";
+import { Button } from "@/components/ui/button";
+import { Timer } from "lucide-react";
+import Link from "next/link";
+
+
+const ExamHeader = () => {
+    return (
+        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <div className="container flex h-16 items-center justify-between">
+                <Link href="/" className="mr-6">
+                    <Logo />
+                </Link>
+                <div className="flex items-center gap-2">
+                    <Timer className="h-5 w-5"/>
+                    <span className="font-semibold">00:29:54</span>
+                </div>
+                <Button>Submit</Button>
+            </div>
+        </header>
+    )
+}
+
+
 export default function TakeExamLayout({
     children,
 }: {
@@ -5,6 +29,7 @@ export default function TakeExamLayout({
 }) {
     return (
         <div className="relative flex min-h-screen flex-col bg-muted/30 dark:bg-gray-900">
+            <ExamHeader />
             <main className="flex-1 container mx-auto py-8">{children}</main>
         </div>
     )
